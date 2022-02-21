99573bbdcebfe0a7b0d23493e721465857ced4d8
luc-dev10 <luc.dev1008@gmail.com>
2022-02-21 12:24:49 -0500

    add employee service and getcustomers in controllers

--- src/main/java/com/lucio/springboot/demo/controller/EmployeeController.java
+++ src/main/java/com/lucio/springboot/demo/controller/EmployeeController.java
@@ -1,9 +1,10 @@
 package com.lucio.springboot.demo.controller;
 
-import com.lucio.springboot.demo.dao.EmployeeDAO;
 import com.lucio.springboot.demo.entity.Employee;
+import com.lucio.springboot.demo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,17 +14,24 @@ import java.util.List;
 @RequestMapping("v1/employees")
 public class EmployeeController {
 
-    // constructor injection of DAO
-    private final EmployeeDAO employeeDAO;
+    // constructor injection of service
+    private final EmployeeService employeeService;
 
     @Autowired
-    public EmployeeController(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
     }
 
     // get employees
     @GetMapping("")
     public List<Employee> getEmployees() {
-        return employeeDAO.getEmployees();
+        return employeeService.getEmployees();
     }
+
+    // get one employee
+    @GetMapping("{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        return employeeService.getEmployee(employeeId);
+    }
+
 }
